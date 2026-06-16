8527d2dae100277b03876e6afd1271018ad1727e
kanikay2012-dotcom <kanika.y2012@gmail.com>
2026-06-16 19:29:32 +0530

    Add files via upload

---
+++ #1.py
@@ -0,0 +1,22 @@
+# Create variables storing the name and birthday of each friend
+friend1_name = "Amit"
+friend1_birthday = "January 14"
+
+friend2_name = "Sneha"
+friend2_birthday = "March 22"
+
+friend3_name = "Rahul"
+friend3_birthday = "July 05"
+
+friend4_name = "Priya"
+friend4_birthday = "September 18"
+
+friend5_name = "Vikram"
+friend5_birthday = "December 30"
+
+# Print the birthdays of all five friends
+print(friend1_name + "'s birthday is on " + friend1_birthday)
+print(friend2_name + "'s birthday is on " + friend2_birthday)
+print(friend3_name + "'s birthday is on " + friend3_birthday)
+print(friend4_name + "'s birthday is on " + friend4_birthday)
+print(friend5_name + "'s birthday is on " + friend5_birthday)
